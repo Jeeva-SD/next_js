3a21a8b4df61dc303028790d7f397f8bcbec820d
Jeeva <jeevasubash64@gmail.com>
2023-01-21 00:50:07 +0530

    lint warning fixes.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,4 @@
-import { getCredentials } from './database';
-import { tabs } from './navigation';
+export { getCredentials } from './database';
+export { tabs } from './navigation';
 
 export const colorArr = ['#EEA47FFF', '#FBEAEB', '#FEE715FF', '#F96167', '#CCF381', '#E2D1F9', '#FCF6F5FF', '#EE4E34', '#FCEDDA', '#ffff', '#EC449B', '#FFBE7BFF'];
-
-export { getCredentials, tabs };
